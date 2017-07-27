11d4c6e079ed67f9204daacca067a1db19228945
daghan <daghan@gmail.com>
2017-07-27 02:12:51 +0200

    added one broken street light data, didn't add all broken street light data. Also fixed merging the crimes and street lights dataframes. Next step will be to add the data for all broken street lights

--- crimes.R
+++ crimes.R
@@ -10,12 +10,12 @@ setwd("/Users/daghanaltas/Hacking/Berkeley/W209/Project/data/")
 #crimes <- read.csv("./Crimes_-_2001_to_present.csv",fileEncoding="latin1")
 crimes <- read.csv("./FinalCrime.csv",fileEncoding="latin1")
 # save as a dataframe
-save(crimes, file = "./crimes_with_zip.Rda")
-#load("./crimes_with_zip.Rda")
+#save(crimes, file = "./crimes_with_zip.Rda")
+load("./crimes_with_zip.Rda")
 
 # convert dates 
-#crimes$Date <- mdy_hms(crimes$Date)
-crimes$Date <- mdy_hm(crimes$Date)
+crimes$Date <- mdy_hms(crimes$Date)
+#crimes$Date <- mdy_hm(crimes$Date)
 
 # filter for 2016 only
 #crimes <- subset(crimes, year(crimes$Date) > 2011)
@@ -40,7 +40,8 @@ tc <- data.frame( #ID = crimes$ID,
                 )
 tc$Occurance <- 1
 sum.crimes.by.date.zip <- as.data.frame(tc  %>% group_by(Date,Zip,Primary.Type) %>% summarise(Sum.Occurance = sum(Occurance))) %>% spread(Primary.Type, Sum.Occurance, fill = 0)
-head(sum.crimes.by.date.zip)
+sum.crimes.by.date.zip$Total.Crimes <- apply(sum.crimes.by.date.zip[,3:dim(sum.crimes.by.date.zip)[2]],1,sum)
+#head(sum.crimes.by.date.zip,100)
 
 
 
@@ -79,12 +80,64 @@ ttt$Broken.Date <- ttt$Creation.Date + ttt$temp
 
 #for each day in each zip code, aggregate the count of total broken alley lights                  
 broken.alley.lights.by.ZIP<- as.data.frame(ttt %>% count(Broken.Date, ZIP.Code))
-colnames(broken.alley.lights.by.ZIP) <- c("Date", "Zip","Broken.Lights")
+colnames(broken.alley.lights.by.ZIP) <- c("Date", "Zip","Broken.Alley.Lights")
 
 # merge it with the per_day, per_zip crime data
-merged_data = merge(x = sum.crimes.by.date.zip, y = broken.alley.lights.by.ZIP, by = c("Date","Zip"), all.y = T)
+merged_data = merge(x = sum.crimes.by.date.zip, y = broken.alley.lights.by.ZIP, 
+                    by = c("Date","Zip"), all.x = T, all.y = T)
 merged_data[is.na(merged_data)] <- 0
 
 # write it out to a CVS file
 write.csv(merged_data, file="./broken_alley_lights_merged_with_crime_2016.csv")
+
+#sort(cor(merged_data[,3:dim(merged_data)[2]])[,32],decreasing = T)
+
+
+
+# adding street lights too
+street.lights <- crimes <- read.csv("./311_Service_Requests_-_Street_Lights_-_One_Out.csv",fileEncoding="latin1")
+save(street.lights, file = "./street_lights.Rda")
+
+street.lights <- street.lights[!grepl("- Dup" , street.lights$Status),]
+street.lights <- street.lights[grepl("Completed" , street.lights$Status),]
+
+# fix the dates
+street.lights$Creation.Date <- mdy(street.lights$Creation.Date)
+street.lights$Completion.Date <- mdy(street.lights$Completion.Date)
+
+# for each completed service, how many dates were a given light was out?
+street.lights$Broken.Days <- street.lights$Completion.Date - street.lights$Creation.Date
+
+#limit the dates to 2016 for now
+street.lights <- subset(street.lights, year(street.lights$Creation.Date) == 2016)
+
+# temporary dataframe for manipulation
+t <- data.frame( Creation.Date = street.lights$Creation.Date,
+                 Completion.Date =  street.lights$Completion.Date,
+                 Broken.Days = as.numeric(street.lights$Broken.Days)+1, 
+                 Service.Request.Number = street.lights$Service.Request.Number,
+                 ZIP.Code = as.factor(street.lights$ZIP.Code),
+                 Community.Area = as.factor(street.lights$Community.Area))
+
+# mega hacks, not even sure where to begin describing
+t$temp <- sapply(t$Broken.Days, function(x) seq(1,x)-1)
+t$temp <- sapply(t$temp, unlist)
+ttt <- separate_rows(t, temp, sep = ",", convert = TRUE)
+ttt$temp <- gsub("^c\\(","",ttt$temp)
+ttt$temp <- gsub("\\)","",ttt$temp)
+ttt$temp <- as.numeric(ttt$temp)
+ttt$Broken.Date <- ttt$Creation.Date + ttt$temp
+
+
+#for each day in each zip code, aggregate the count of total broken alley lights                  
+broken.street.lights.by.ZIP<- as.data.frame(ttt %>% count(Broken.Date, ZIP.Code))
+colnames(broken.street.lights.by.ZIP) <- c("Date", "Zip","Broken.Street.Lights")
+
+
+merged_data2 = merge(x = merged_data, y = broken.street.lights.by.ZIP, 
+                     by = c("Date","Zip"), all.x = T, all.y = T)
+merged_data2[is.na(merged_data2)] <- 0
+
+write.csv(merged_data2, file="./broken_alley_and_street_lights_merged_with_crime_2016.csv")
+
 
